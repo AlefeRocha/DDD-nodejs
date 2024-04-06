45f6f1b541a56b46e83ce5f0b854d6e0de496438
Alefe Rocha <alefe.rtavares@gmail.com>
2024-04-06 15:37:09 -0300

    more entities

--- src/domain/use-cases/answer-question.ts
+++ src/domain/use-cases/answer-question.ts
@@ -1,5 +1,16 @@
+interface AnswerQuestionUseCaseRequest {
+  instructorId: string
+  questionId: string
+}
+
 export class AnswerQuestionUseCase {
-  handler() {
-    
+  
+  handler({ instructorId, questionId }: AnswerQuestionUseCaseRequest) {
+
   }
 }
+
+new AnswerQuestionUseCase().handler({
+  instructorId: '1',
+  questionId: '2'
+})
