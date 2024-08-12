8c378b4ff8e7272052c9f4e3fd89f857e90876d8
Alefe Rocha <alefe.rtavares@gmail.com>
2024-08-12 18:24:15 -0300

    a

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -1,21 +1,18 @@
-import { randomUUID } from "node:crypto"
+import { Entity } from "../../core/entities/entity"
 
 interface AnswerProps {
   content: string
   authorId: string
   questionId: string
+  createdAt: Date
+  updatedAt?: Date
 }
 
-export class Answer {
-  public id: string
-  public content: string
-  public authorId: string
-  public questionId: string
-
+export class Answer extends Entity<AnswerProps> {
+  get content() {
+    return this.props.content
+  }
   constructor(props: AnswerProps, id?: string) {
-    this.content = props.content
-    this.authorId = props.authorId
-    this.questionId = props.questionId
-    this.id = id ?? randomUUID()
+    super(props, id)
   }
 }
