51a3521bbda70d6219626bc32fa4e83105ca3738
Alefe Rocha <alefe.rtavares@gmail.com>
2024-08-12 19:27:27 -0300

    creating core folder with entities and id generator function

--- src/domain/entities/answer.ts
+++ src/domain/entities/answer.ts
@@ -12,7 +12,4 @@ export class Answer extends Entity<AnswerProps> {
   get content() {
     return this.props.content
   }
-  constructor(props: AnswerProps, id?: string) {
-    super(props, id)
-  }
 }
